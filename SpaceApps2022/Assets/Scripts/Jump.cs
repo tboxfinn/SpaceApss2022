2f13a12cbf43b6d373468271c8577bdca68138f3
HugoVD <104853753+tboxfinn@users.noreply.github.com>
2022-10-01 13:25:21 -0500

    saltoalto

--- SpaceApps2022/Assets/Scripts/Jump.cs
+++ SpaceApps2022/Assets/Scripts/Jump.cs
@@ -6,11 +6,19 @@ public class Jump : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float speed;
+    public float jumpForce;
     private float moveInput;
+   
 
     private bool isGrounded;
     public Transform feetPos;
     public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private float jumpTimeCounter;
+    public float jumpTime;
+    private bool isJumping;
+
 
     private void Start()
     {
@@ -25,6 +33,43 @@ public class Jump : MonoBehaviour
 
     void Update()
     {
-        isGrounded = 
+        isGrounded = Physics2D.OverlapCircle(feetPos.position,checkRadius,whatIsGround);
+
+        if (moveInput > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            if (moveInput < 0)
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+        }
+
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isJumping == true)
+        {
+            if(jumpTimeCounter > 0)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+            
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
     }
 }
