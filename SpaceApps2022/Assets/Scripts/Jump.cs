a73b7bf51293ae1ac0fe7258cd5fcd0368e994cd
HugoVD <104853753+tboxfinn@users.noreply.github.com>
2022-10-01 12:53:10 -0500

    j8ump

--- SpaceApps2022/Assets/Scripts/Jump.cs
+++ SpaceApps2022/Assets/Scripts/Jump.cs
@@ -8,13 +8,23 @@ public class Jump : MonoBehaviour
     public float speed;
     private float moveInput;
 
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
+
     void FixedUpdate()
     {
         moveInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+    void Update()
+    {
+        isGrounded = 
     }
 }
