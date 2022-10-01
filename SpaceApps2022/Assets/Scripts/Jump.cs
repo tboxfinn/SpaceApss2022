d86fe3a1a69f6b4cdd51066ccb9bb929286a00fe
HugoVD <104853753+tboxfinn@users.noreply.github.com>
2022-10-01 12:48:11 -0500

    movpersonaje
    ,..

---
+++ SpaceApps2022/Assets/Scripts/Jump.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+    private float moveInput;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+}
