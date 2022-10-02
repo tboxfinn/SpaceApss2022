b6d650f7631b3134f786b055cc74ccc35f3a61ee
HugoVD <104853753+tboxfinn@users.noreply.github.com>
2022-10-02 12:26:28 -0500

    mi laa

---
+++ SpaceApps2022/Assets/Scripts/PlataformaSube.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaSube : MonoBehaviour
+{
+    public GameObject ObjetoAMover;
+
+    public Transform StartPoint;
+    public Transform EndPoint;
+
+    public float Velocidad;
+
+    private Vector3 MoverHacia;
+
+    void Start()
+    {
+        MoverHacia = EndPoint.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ObjetoAMover.transform.position = Vector3.MoveTowards(ObjetoAMover.transform.position, MoverHacia, Velocidad * Time.deltaTime);
+
+        if (ObjetoAMover.transform.position == EndPoint.position)
+        {
+            MoverHacia = StartPoint.position;
+        }
+
+        if (ObjetoAMover.transform.position == StartPoint.position)
+        {
+            MoverHacia = EndPoint.position;
+        }
+    }
+}
