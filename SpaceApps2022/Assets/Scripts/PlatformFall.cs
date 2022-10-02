37eda1b6f99dc8d31c46f3a82cc85dc55918ca6c
Eric Zaleta <ericzaleta@gmail.com>
2022-10-02 21:06:53 +0100

    volume

--- SpaceApps2022/Assets/Scripts/PlatformFall.cs
+++ SpaceApps2022/Assets/Scripts/PlatformFall.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 public class PlatformFall : MonoBehaviour
 {
     Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private BoxCollider2D bc;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        bc = GetComponent<BoxCollider2D>();
     }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name.Equals("PLAYER"))
         {
             Invoke("DropPlatform", 2.5f);
-            Destroy(gameObject, 3f);
+
         }
     }
 
     void DropPlatform()
     {
-        rb.isKinematic = false;
+        //rb.isKinematic = false;
+        sr.color = new Color(1, 1, 1, 0.0f);
+        bc.enabled = false;
     }
 }
