e238df79b3ca300e0e6d2a46ef2ea0123425435b
CharloHdz <84859398+CharloHdz@users.noreply.github.com>
2022-10-01 23:37:00 -0500

    James Webb
    Faltan cosas del canvas (botones del panel de menu y pausa) y algunos assets

--- SpaceApps2022/Assets/Scripts/GameManager.cs
+++ SpaceApps2022/Assets/Scripts/GameManager.cs
@@ -8,18 +8,22 @@ public class GameManager : MonoBehaviour
 {
 
     //PanelControl
-    [SerializeField] private TextMeshProUGUI Title_Text, Description_Text, TitleText2, DescriptionText2;
-    //images Panel
-    public GameObject panel, PanelContent, PanelWin, image, image2,SecondImage, SecondImage2, Congratulations;
-    public static bool OnPanel;
-    public Animator Panel_anim;
+    [SerializeField] private TextMeshProUGUI Title_Text, Description_Text, TitleText2, DescriptionText2, piecesText;
     public int pieces;
+ 
+    //images Panel & Game Panel
+    public GameObject GamePanel, MenuPanel, PausePanel, PanelWin, image, image2,SecondImage, Congratulations, panel, PanelContent;
+    public static bool OnPanel, OnPause, OnMenu, OnGame;
+    public Animator Panel_anim;
     // Start is called before the first frame update
     void Start()
     {
-
+        MenuPanel.SetActive(true);
 
         //PanelControl
+        panel.SetActive(false);
+        PanelContent.SetActive(false);
+        PanelWin.SetActive(false);
 
     }
 
@@ -29,6 +33,21 @@ public class GameManager : MonoBehaviour
         if (pieces == 7)
         {
             Congratulations.SetActive(true);
+        }
+        else if (pieces >= 8)
+        {
+            piecesText.text = "7/7";
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape) && OnPause ==true)
+        {
+            PausePanel.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && OnPause == false)
+        {
+            PausePanel.SetActive(false);
+            Time.timeScale = 1;
         }
     }
 
@@ -52,17 +71,42 @@ public class GameManager : MonoBehaviour
             TitleText2.text = data.itemName;
             DescriptionText2.text = data.itemDescription;
             SecondImage.GetComponent<Image>().sprite = data.itemImage;
-            SecondImage2.GetComponent<Image>().sprite = data.itemImage2;
 
         }
+        piecesText.text = pieces + "/7";
+        GamePanel.SetActive(false);
 
     }
     public void Closedisplay()
     {
         panel.SetActive(false);
         PanelContent.SetActive(false);
+        PanelWin.SetActive(false);
 
         Time.timeScale = 1;
         OnPanel = false;
+        GamePanel.SetActive(true);
+    }
+
+    public void Pause()
+    {
+        if (OnPause == true)
+        {
+            Game();
+        }
+        else if (OnPause == false)
+        {
+            PausePanel.SetActive(true);
+        }
+    }
+
+    public void Game()
+    {
+
+    }
+
+    public void Menu()
+    {
+
     }
 }
