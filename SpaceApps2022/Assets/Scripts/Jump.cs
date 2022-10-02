e594e3a215a3f4ab3d02f68cbd59b0ed083fa1fd
Eric Zaleta <ericzaleta@gmail.com>
2022-10-02 21:18:13 +0100

    Revert "Merge branch 'Development' into TestingCharlo"
    This reverts commit 76435d281e0131ff032b4b4f997c03b556450245, reversing
    changes made to e738ba0d8f75048830a6c9ec0f2401217dec6196.

--- SpaceApps2022/Assets/Scripts/Jump.cs
+++ SpaceApps2022/Assets/Scripts/Jump.cs
@@ -6,26 +6,15 @@ public class Jump : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float speed;
-    public float jumpForce;
     private float moveInput;
 
-
-    public bool isGrounded;
+    [SerializeField] private GameManager gameManager;
     public Transform feetPos;
     public float checkRadius;
-    public LayerMask whatIsGround;
-
-    private float jumpTimeCounter;
-    public float jumpTime;
-    private bool isJumping;
-
-    public bool preJump = false;
-    public PhysicsMaterial2D bounceMat, normalMat;
-
-    private float direction = 1;
-    [SerializeField] private float limite;
-
-
+    //PanelControl
+    [SerializeField] float JumpForce;
+    //imagespanel
+    public static int index;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -33,77 +22,27 @@ public class Jump : MonoBehaviour
 
     void FixedUpdate()
     {
-        PlayerMove();
-        Flip();
-        PlayerJump();
-    }
-
-    private void PlayerMove()
-    {
         moveInput = Input.GetAxisRaw("Horizontal");
-        if (isGrounded && !preJump)
-            rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
-    }
-    private void Flip()
-    {
-        if (moveInput > 0)
-        {
-            transform.eulerAngles = new Vector3(0, 0, 0);
-        }
-        else
-        {
-            if (moveInput < 0)
-            {
-                transform.eulerAngles = new Vector3(0, 180, 0);
-            }
-        }
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
     }
 
-    private void PlayerJump()
+    void Update()
     {
 
-        direction = Input.GetAxisRaw("Horizontal");
-
-        if (isGrounded == true && preJump)
+        if (Input.GetKeyDown(KeyCode.Space) && GameManager.OnPanel == false)
         {
-            jumpForce += 12f * Time.deltaTime;
-            rb.velocity = new Vector2(0, rb.velocity.y);
-            rb.sharedMaterial = bounceMat;
-
-        }
-        if (Input.GetKey(KeyCode.Space) && jumpForce < limite)
-        {
-
-            preJump = true;
-        }
-        else if (preJump || jumpForce >= limite)
-        {
-            preJump = false;
-
-            rb.AddForce(new Vector2(direction, 1) * jumpForce, ForceMode2D.Impulse);
-
-        }
-
-        if (rb.velocity.y <= -1)
-        {
-
-
-            rb.sharedMaterial = normalMat;
-        }
-        if (rb.velocity.y >= 1)
-        {
-            jumpForce = 0;
+            rb.AddForce(Vector2.up * JumpForce);
         }
     }
-    private void ResetJump()
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        jumpForce = 0.0f;
+        if (collision.gameObject.CompareTag("InfoItem"))
+        {
+            gameManager.Display(collision.gameObject.GetComponent<DataDisplay>().data);
+
+        }
     }
 
-    private void Update()
-    {
-        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
-    }
 
-    
 }
